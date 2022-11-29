515c732830dedc27da9cbaa1d450cdea9fa973c6
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-11-29 21:46:40 +0200

    added contacts function

--- contacts.js
+++ contacts.js
@@ -3,19 +3,54 @@ const fs = require("fs/promises");
 
 const contactsPath = path.resolve("db/contacts.json");
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const dataParse = JSON.parse(data);
+    return dataParse;
+  } catch (error) {
+    console.log(
+      "🚀 ~ file: contacts.js ~ line 12 ~ listContacts ~ error",
+      error
+    );
+  }
 }
 
 function getContactById(contactId) {
-  // ...твій код
+   try {
+    const data = await listContacts();
+    const contact = data.find(({ id }) => id === contactId.toString());
+    console.log(`Contact with id ${contactId} is: ${JSON.stringify(contact)}`);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function removeContact(contactId) {
-  // ...твій код
+  try {
+    const data = await listContacts();
+    const newData = data.filter(({ id }) => id !== contactId.toString());
+    console.log(`Contact with ${contactId} successfully deleted`);
+    await fs.writeFile(contactsPath, JSON.stringify(newData));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function addContact(name, email, phone) {
-  // ...твій код
+    try {
+    const data = await listContacts();
+    data.push({ name, email, phone });
+    await fs.appendFile(contactsPath, JSON.stringify(data));
+    console.log(`Contact ${name} added`);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-const { getCurrentDate } = require("./dateUtils");
-console.log(`get current date: ${getCurrentDate()}`);
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
+async function fileOperation({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const data = await listContacts();
+      console.table(data);
+      break;
+    case "get":
+      await getContactById(id);
+      break;
+
+    case "add":
+      await addContact(name, email, phone);
+      break;
+
+    case "remove":
+      await removeContact(id);
+      break;
+
+    default:
+      console.log("Unknown action type!");
+  }
+}
+
+fileOperation({ action: "list" });
